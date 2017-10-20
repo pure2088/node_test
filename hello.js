e6fe23e39e56b6621dc27966b308b00711ee2551
nick <pure2088@gmail.com>
2017-10-20 20:04:41 +0800

    init commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+var http = require('http');  
+http.createServer(function handler(request, respose) {  
+    respose.writeHead(200, {  
+        'Content-Type' : 'text/html'  
+    });  
+    respose.end('Hello World\n');  
+}).listen(8888,'test.node.com');  
+console.log("服务器已经启动，地址为：http://test.node.com:8888");
