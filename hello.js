6a308d5d74bdb547bc5b699668d0be6bf39614a7
nick <pure2088@gmail.com>
2017-10-22 14:53:49 +0800

    update hello.js test

--- hello.js
+++ hello.js
@@ -3,6 +3,6 @@ http.createServer(function handler(request, respose) {
     respose.writeHead(200, {  
         'Content-Type' : 'text/html'  
     });  
-    respose.end('Hello World88\n');  
+    respose.end('Hello World888\n');  
 }).listen(8888,'test.node.com');  
 console.log("服务器已经启动，地址为：http://test.node.com:8888");
